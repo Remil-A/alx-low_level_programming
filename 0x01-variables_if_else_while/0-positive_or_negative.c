29a1dde1c28191f8b964e53c63238c1a3972274a
Remil-A <adusarah531@gmail.com>
2022-03-14 16:59:23 -0700

    Variables, if statements and loops

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
-/**
- *  main - assigns a random number to int n everytime
- *  it executes, and prints it
- *  Return: Always 0 (Success)
+/** main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -15,11 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	        printf ("%d is positive\n", n);
+		printf("%d is positive\n"' n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
-	return (0);
 }
 
